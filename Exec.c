3dc6bdf0fc86dfcbcf6f7a167a56edadfb1ee44b
ShehabAldeenMo <shihab.rifai02@eng-st.cu.edu.eg>
2024-07-29 14:17:10 +0300

    Initial Commit

---
+++ Exec.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+
+
+
+int main(){
+	char charachter = getchar();
+	char* const Arglist[3] = {"sleep","10",NULL} ;
+	char* const Envlist[] = {NULL};
+	int ret = execve ("/usr/bin/sleep",Arglist,Envlist);
+	return 0 ;
+}
---
+++ fork.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+int global = 10;
+
+int main(int argc, char ** argv)
+{
+	printf("current PID = %d\n", getpid());
+        
+	int retPid = fork();
+	int local = 100;
+	global+= 20;
+	if (retPid > 0)
+	{
+		printf("This is the parent\n the current PID = %d, the child pid = %d\n", getpid(), retPid);
+		
+		global++;
+		local++;
+
+		while(1)
+		{
+
+		printf("global = %d, local = %d\n", global, local);
+		int status =0;	
+		int childPid = wait(&status);
+
+
+		printf("exit status = %d\n",WEXITSTATUS(status));
+		printf("Child PID = %d\n",childPid);
+		sleep(5);
+		}
+	}
+	else if (retPid == 0)
+	{
+
+		int i =0;
+		for( i =0; i < 3 ; i++)
+		{
+
+		printf("This is the Child\n, current PID = %d\n", getpid());
+		printf("global = %d, local = %d\n", global, local);
+		sleep(5);
+		}
+		exit(100);
+	}
+	else
+	{
+
+		perror("fork");
+	}
+}
